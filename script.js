e3a1d0254f3e67b1c954d2428953c6e6a63c6d64
Kezya <kjpilao22@src.pshs.edu.ph>
2025-02-24 12:03:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function condiFunc() {
+    let message = prompt('Enter a message: ');
+
+    if (message.includes("FR") && message.includes("AI")) {
+        document.getElementById('result').innerHTML = "The message is legitimate.";
+    } 
+    else if (message.includes("AI") || message.includes("aI") || message.includes("Ai")) {
+        document.getElementById('result').innerHTML ="The message is tampered with.";
+    } 
+    else if (message.includes("FR")) {
+         document.getElementById('result').innerHTML = "The message is legitimate.";
+    } 
+    else {
+         document.getElementById('result').innerHTML ="The message is yet to be encoded.";
+    }
+}
