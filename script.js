ddf26d3f69465478faaa5ce8686f11bae6b217b1
Kezya <kjpilao22@src.pshs.edu.ph>
2025-03-02 17:59:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ function condiFunc() {
     let message = prompt('Enter a message: ');
 
     if (message.includes("FR") && message.includes("AI")) {
-        document.getElementById('result').innerHTML = "The message is legitimate.";
+        document.getElementById('result').innerHTML = `The message "${message}" is legitimate.`;
     } 
     else if (message.includes("AI") || message.includes("aI") || message.includes("Ai")) {
-        document.getElementById('result').innerHTML ="The message is tampered with.";
+        document.getElementById('result').innerHTML =`The message "${message}" is tampered with.`;
     } 
     else if (message.includes("FR")) {
-         document.getElementById('result').innerHTML = "The message is legitimate.";
+         document.getElementById('result').innerHTML = `The message "${message}" is legitimate.`;
     } 
     else {
-         document.getElementById('result').innerHTML ="The message is yet to be encoded.";
+         document.getElementById('result').innerHTML =`The message "${message}" is yet to be encoded.`;
     }
 }
